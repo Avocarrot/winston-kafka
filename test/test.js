0f006db385cbb187b25f4d70a0be068ff26f38cc
Panos <panos@avocarrot.com>
2015-09-17 19:20:24 +0300

    Changed assertion order

--- test/test.js
+++ test/test.js
@@ -12,12 +12,12 @@ var client,
 describe('Winston kafka transport', function() {
   it('should be able to initialize without any arguments', function() {
     var kafkaTransport = new transport.Kafka();
-    assert.equal('object', typeof kafkaTransport);
+    assert.equal(typeof kafkaTransport, 'object');
   });
 
   it('name should be kafka', function() {
     var kafkaTransport = new transport.Kafka();
-    assert.equal('kafka', kafkaTransport.name);
+    assert.equal(kafkaTransport.name, 'kafka');
   });
 
   it('should return an error if a message is sent before producer is ready', function() {
