e734234be4c09d60f7f94a2c0621e6274ac21b80
Panos <panos@avocarrot.com>
2015-09-17 19:19:17 +0300

    Added one more test

--- test/test.js
+++ test/test.js
@@ -3,6 +3,12 @@ var winston = require('winston');
 var kafka = require('kafka-node');
 var transport = require('../index.js');
 
+var client,
+  producer,
+  zkHost = 'localhost:2181',
+  topic = 'kafka-transport-test',
+  clientId = 'winston-kafka-transport-test';
+
 describe('Winston kafka transport', function() {
   it('should be able to initialize without any arguments', function() {
     var kafkaTransport = new transport.Kafka();
@@ -12,5 +18,19 @@ describe('Winston kafka transport', function() {
   it('name should be kafka', function() {
     var kafkaTransport = new transport.Kafka();
     assert.equal('kafka', kafkaTransport.name);
+  });
+
+  it('should return an error if a message is sent before producer is ready', function() {
+    var options = {
+      topic: topic
+    };
+    var kafkaTransport = new transport.Kafka(options);
+    var message = {
+      msg: 'Testing',
+      level: 'error'
+    };
+    kafkaTransport._send(message, function(error){
+      assert.equal(error instanceof Error, true);
+    });
   });
 });
